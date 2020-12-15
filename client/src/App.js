8b1d90bdd86e33770c4d0d5e67c61092a6edf0d5
david <davidrwilson3@gmail.com>
2020-12-15 17:55:20 -0500

    added name header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import "./styles/App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import ChoosePatient from "./views/ChoosePatient";
 
 const App = () => {
     return (
-        <div className='App'>
-            <ChoosePatient />
-        </div>
+        <>
+            <div className={"header-bar"}>Created by David Wilson (2020)</div>
+            <div className='App'>
+                <ChoosePatient />
+            </div>
+        </>
     );
 };
 
