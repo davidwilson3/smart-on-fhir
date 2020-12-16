3614eaac80d2407a637ae731867c53162a248c36
david <davidrwilson3@gmail.com>
2020-12-15 21:26:11 -0500

    add basic tests

--- server/tests/formatCernerData.test.js
+++ server/tests/formatCernerData.test.js
@@ -5,11 +5,19 @@ const { ERROR_MSG } = require("../services/constants");
 
 test("Verify formatPatientData.js correctly formats Cerner patient data", () => {
     const outputData = formatPatientData("cerner", cernerInput);
+
+    // test keys
+    expect(outputData).toHaveProperty("name", "gender", "birthDate", "conditions");
+
+    // test regression from expected output
     expect(outputData).toMatchObject({
         name: cernerOutput["name"],
         gender: cernerOutput["gender"],
         birthDate: cernerOutput["birthDate"],
     });
+
     expect(outputData.conditions.length).toBe(cernerOutput.conditions.length);
+
+    // test error message
     expect(formatPatientData("Does Not Exist", cernerInput)).toBe(ERROR_MSG);
 });
--- server/tests/formatSmartHealthData.test.js
+++ server/tests/formatSmartHealthData.test.js
@@ -5,11 +5,19 @@ const { ERROR_MSG } = require("../services/constants");
 
 test("Verify formatPatientData.js correctly SmartHealth patient data", () => {
     const outputData = formatPatientData("SmartHealth", smartHealthInput);
+
+    // test keys
+    expect(outputData).toHaveProperty("name", "gender", "birthDate", "conditions");
+
+    // test regression from expected output
     expect(outputData).toMatchObject({
         name: smartHealthOutput["name"],
         gender: smartHealthOutput["gender"],
         birthDate: smartHealthOutput["birthDate"],
     });
+
     expect(outputData.conditions.length).toBe(smartHealthOutput.conditions.length);
+
+    // test error message
     expect(formatPatientData("Does Not Exist", smartHealthInput)).toBe(ERROR_MSG);
 });
